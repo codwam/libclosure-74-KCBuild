171361787d2f64d33ba01fe18f75a71cba312e8f
lihui02 <lihui02@lizhi.fm>
2021-06-02 09:14:43 +0800

    test

--- libclosure-74/KCBlockDemo/main.m
+++ libclosure-74/KCBlockDemo/main.m
@@ -7,14 +7,104 @@
 
 #import <Foundation/Foundation.h>
 
+typedef void(^VoidBlock)(void);
+
+// MARK: - global
+
+static void test_global(void) {
+    VoidBlock block = ^{
+        NSLog(@"Hello, World!");
+    };
+    NSLog(@"%@", [block class]);
+    block();
+    
+    /*
+     编译的时候是_NSConcreteStackBlock，但调试的时候是__NSGlobalBlock__
+     */
+}
+
+// MARK: - stack
+
+static void test_stack(void) {
+    int a = 10;
+    NSLog(@"%@", [^{
+        NSLog(@"%d", a);
+    } class]);
+    
+    /*
+     __NSStackBlock__， 根本不走_Block_copy
+     */
+}
+
+// MARK: - malloc
+
+static void test_malloc(void) {
+    int a = 10;
+    VoidBlock block = ^{
+        NSLog(@"%d", a);
+    };
+    NSLog(@"%@", [block class]);
+    block();
+    
+    /*
+     __NSMallocBlock__，没有copy和dispose函数。
+     */
+}
+
+static void test_malloc_2(void) {
+    NSObject* obj = [NSObject new];
+    VoidBlock block = [^{
+        NSLog(@"%@", obj);
+    } copy];
+    NSLog(@"%@", [block class]);
+    block();
+    
+    /*
+     __NSMallocBlock__，有copy和dispose函数。但是没有调用_Block_object_assign和_Block_object_dispose函数。
+     */
+}
+
+static void test_malloc_3(void) {
+    __block int a = 10;
+    VoidBlock block = [^{
+        NSLog(@"%d", a);
+    } copy];
+    NSLog(@"%@", [block class]);
+    block();
+    
+    /*
+     __NSMallocBlock__，有copy和dispose函数。有调用_Block_object_assign和_Block_object_dispose函数。
+     */
+}
+
+static void test_malloc_4(void) {
+    __block NSObject* obj = [NSObject new];
+    VoidBlock block = [^{
+        NSLog(@"%@", obj);
+    } copy];
+    NSLog(@"%@", [block class]);
+    block();
+    
+    /*
+     __NSMallocBlock__，有copy和dispose函数。但是没有调用_Block_object_assign和_Block_object_dispose函数。
+     */
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        __block int a = 10;
-        void (^mallocBlock)(void) = ^void { a++; };
-        NSLog(@"MallocBlock is %@", mallocBlock);
+//        __block int a = 10;
+//        void (^mallocBlock)(void) = ^void { a++; };
+//        NSLog(@"MallocBlock is %@", mallocBlock);
+//
+//        NSLog(@"Hello, World!");
         
-        NSLog(@"Hello, World!");
+//        test_global();
+//        test_stack();
+//        test_malloc();
+//        test_malloc_2();
+//        test_malloc_3();
+        test_malloc_4();
     }
     return 0;
 }
